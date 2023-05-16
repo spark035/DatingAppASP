53895656936a34a49ff6ac9f12acf26d860c741a
spark035 <63474921+spark035@users.noreply.github.com>
2023-05-16 15:12:31 -0400

    register hooked, login and home with nav created

--- API/Program.cs
+++ API/Program.cs
@@ -19,7 +19,8 @@ var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 app.UseCors(builder =>
-  builder.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
+  builder.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200")
+);
 
 app.UseAuthentication();
 app.UseAuthorization();
