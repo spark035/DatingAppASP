400a2a60fab650ea70121a0d91d31ab92da2723b
spark035 <63474921+spark035@users.noreply.github.com>
2023-05-25 21:55:54 -0400

    Pagination, filters and caching

---
+++ API/Helpers/PaginationHeader.cs
@@ -0,0 +1,19 @@
+namespace API.Helpers
+{
+    public class PaginationHeader
+    {
+        public PaginationHeader(int currentPage, int itemsPerPage, int totalItems, int totalPages)
+        {
+            TotalPages = totalPages;
+            ItemsPerPage = itemsPerPage;
+            TotalItems = totalItems;
+            TotalPages = totalPages;
+        }
+
+        public int CurrentPage { get; set; }
+        public int ItemsPerPage { get; set; }
+        public int TotalItems { get; set; }
+        public int TotalPages { get; set; }
+
+    }
+}
