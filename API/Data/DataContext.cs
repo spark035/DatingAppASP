1595bdbe92a99c65be768f8bfab13f2cdb65abf3
spark035 <63474921+spark035@users.noreply.github.com>
2023-05-27 14:34:48 -0400

    likes with pagination

--- API/Data/DataContext.cs
+++ API/Data/DataContext.cs
@@ -3,12 +3,33 @@ using Microsoft.EntityFrameworkCore;
 
 namespace API.Data
 {
-  public class DataContext : DbContext
-  {
-    public DataContext(DbContextOptions options) : base(options)
+    public class DataContext : DbContext
     {
-    }
+        public DataContext(DbContextOptions options) : base(options)
+        {
+        }
 
-    public DbSet<AppUser> Users { get; set; }
-  }
+        public DbSet<AppUser> Users { get; set; }
+        public DbSet<UserLike> Likes { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<UserLike>()
+              .HasKey(k => new { k.SourceUserId, k.TargetUserId });
+
+            modelBuilder.Entity<UserLike>()
+              .HasOne(s => s.SourceUser)
+              .WithMany(l => l.LikedUsers)
+              .HasForeignKey(s => s.SourceUserId)
+              .OnDelete(DeleteBehavior.Cascade);
+
+            modelBuilder.Entity<UserLike>()
+              .HasOne(s => s.TargetUser)
+              .WithMany(l => l.LikedByUsers)
+              .HasForeignKey(s => s.TargetUserId)
+              .OnDelete(DeleteBehavior.Cascade);
+        }
+    }
 }
